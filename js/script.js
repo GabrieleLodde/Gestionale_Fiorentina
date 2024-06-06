24c6a0543bf708a320962c227b4f80b824603f89
Gabriele Lodde <gabrilodde@gmail.com>
2024-06-07 00:51:36 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
 $(function(){
     var note = $('#note');
     // Imposta la data desiderata: 17 agosto 2024
-    var ts = new Date(2024, 7, 17, 15, 0, 0);
-    
-    /*if((new Date()) > ts){
-        // La data e l'ora specificate sono già passate, aggiorno il ts per mostrare un altro countdown
-        // Ad esempio, aggiornato di 10 giorni
-        ts.setDate(ts.getDate() + 10);
-    }*/
+    var ts = new Date(2024, 7, 17, 15);
         
     $('#countdown').countdown({
         timestamp: ts,
@@ -18,7 +12,7 @@ $(function(){
             message += days + " day" + (days == 1 ? '' : 's') + " and ";
             message += hours + " hour" + (hours == 1 ? '' : 's') + "<br /> ";
             
-            // Aggiungi il messaggio al div con l'id "note"
+            // Aggiungi il messaggio al div
             note.html(message);
         }
     });
